4c9e7743676f587fa5046e40197d90b038cb18e7
amnzr <aman.zare@gmail.com>
2023-06-19 22:51:12 +0200

    added select input for section and sample_by chnaged the choices to be read from data enclosed in shinyUI function

--- ui.R
+++ ui.R
@@ -14,14 +14,21 @@ header <- dashboardHeader(
 
 ## Side bar
 sidebar <- dashboardSidebar(
-  selectInput("sampled_by", "Sampled By",
-              choices = c("Aman", "Tiggy", "Nazeer", "Miko"),
-              multiple = TRUE),
   selectInput("section", "Section(s)",
-              choices = c("A","B","D"),
+              choices = c(data$Section %>%
+                            unique()),
+              multiple = TRUE),
+  # conditionalPanel(condition = "input.section.length > 0",
+  selectInput("sampled_by", "Sampled By",
+              choices = data$Sampled_by %>%
+                unique() %>%
+                str_split("_") %>%
+                unlist() %>%
+                unique(),
               multiple = TRUE),
   dateRangeInput("dates", "Sampling dates:", start = NULL, end = NULL)
 )
+# )
 
 ## body
 body <- dashboardBody(tags$head(
@@ -33,11 +40,12 @@ tabsetPanel(
   tabPanel("About",includeMarkdown("README.md")))
 )
 
-
+shinyUI(
 # putting ui parts together
 dashboardPage(
   title = "Aspen root collection",
   header = header,
-  sidebar = sidebar ,
+  sidebar = sidebar,
   body = body
+)
 )
