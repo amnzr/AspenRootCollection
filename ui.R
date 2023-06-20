38d5d0a77245d84842fb5af041ae61149a4cfc99
amnzr <aman.zare@gmail.com>
2023-06-20 23:44:34 +0200

    added bslib and bsicons packages changed the ui style from shinydashboard to bslib addes 2 dummy value boxes

--- ui.R
+++ ui.R
@@ -1,33 +1,35 @@
+library(bsicons)
+library(bslib)
 library(DT)
 library(leaflet)
 library(shiny)
 library(shinydashboard)
 library(stringr)
 
-# ui parts
-## header board
-header <- dashboardHeader(
-  title = 'Aspen Root Sampling')
+page_sidebar(theme = bs_theme(secondary = "#21719c"),
+             sidebar = uiOutput("sidebar"),
+             title = "Aspen root collection",
+             layout_column_wrap(
+               width = "250px", fill = FALSE,height = "100px",
+               value_box(
+                 title = "Number of seleted Trees",
+                 value = 34,
+                 showcase = bs_icon("tree"),
+                 theme_color = "secondary"
+               ),
+               value_box(
+                 title = "Number of Collcetors",
+                 value = 2,
+                 showcase = bs_icon("person-heart"),
+                 theme_color = "secondary"
+               )
+             ),
 
-## Side bar
-sidebar <- uiOutput("sidebar")
+             tabsetPanel(
+               tabPanel("Map", leafletOutput("map", height=1000)),
+               tabPanel("Data", dataTableOutput("table")),
+               tabPanel("About",includeMarkdown("README.md"))
+             )
 
-## body
-body <- dashboardBody(tags$head(
-  tags$style(HTML(".skin-blue .main-header .logo"))
-),
-tabsetPanel(
-  tabPanel("Map", leafletOutput("map", height=1000)),
-  tabPanel("Data", dataTableOutput("table")),
-  tabPanel("About",includeMarkdown("README.md")))
-)
 
-shinyUI(
-# putting ui parts together
-dashboardPage(
-  title = "Aspen root collection",
-  header = header,
-  sidebar = sidebar,
-  body = body
-)
 )
