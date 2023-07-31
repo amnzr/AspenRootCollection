efa11526c4359f2fdb58e14836b5fe0dae8b4dad
amnzr <aman.zare@gmail.com>
2023-07-31 09:33:29 +0200

    commented out trimming wrong data section (since the input is cleaner now) added Shipping_date to leaflet popups

--- server.R
+++ server.R
@@ -2,6 +2,7 @@
 library(bsTools)
 library(DT)
 library(leaflet)
+library(lubridate)
 library(magrittr)
 library(Polychrome)
 library(RColorBrewer)
@@ -14,19 +15,20 @@ library(tidyverse)
 #the server logic
 shinyServer(function(input, output, session) {
   #reading input
-  data <- read.csv("data/NordAsp_root_coordinates.csv", strip.white = TRUE) %>%
+  data <- read.csv("data/NordAsp_root_coordinates.csv") %>%
     type.convert(as.is = TRUE) %>%
     as_tibble()
 
   #convert coordinates to numeric
   data$Longitude <- as.numeric(data$Longitude) %>% suppressWarnings()
   data$Latitude <- as.numeric(data$Latitude) %>% suppressWarnings()
+  data$Sampling_date <- as.Date(data$Sampling_date)
 
-  #trimming the wrong format coordinates. Kipping only if they are in this format: dd.ddddd
-  data <- data[grepl("^\\d+\\.\\d+$", data$Latitude) | grepl("^\\d+\\.\\d+$", data$Longitude), ]
-
-  #remove the wrong and strange coordinates
-  data %<>% filter(!Tree %in% c("R32", "K6","K9", "E26"))
+  # #trimming the wrong format coordinates. Kipping only if they are in this format: dd.ddddd
+  # data <- data[grepl("^\\d+\\.\\d+$", data$Latitude) | grepl("^\\d+\\.\\d+$", data$Longitude), ]
+  #
+  # #remove the wrong and strange coordinates
+  # data %<>% filter(!Tree %in% c("R32", "K6","K9", "E26"))
 
   #extract the section and set as new column
   data %<>% mutate(Section = str_match(Tree, "^([A-Za-z]+)")[,1], .after = Tree)
@@ -99,7 +101,8 @@ shinyServer(function(input, output, session) {
                        label = ~Tree,
                        popup = ~paste("<strong>", Tree, "</strong>",
                                       "<br>Sampled_by:", Sampled_by,
-                                      "<br>Sampling_date:", Sampling_date),
+                                      "<br>Sampling_date:", Sampling_date,
+                                      "<br>Shipping_date:", Shipping_date),
                        color = ~pal()(data_filter()$Section),
                        fillOpacity = 0.8) %>%
       addLegend(pal=pal(),
@@ -206,7 +209,8 @@ shinyServer(function(input, output, session) {
                        label = ~Tree,
                        popup = ~paste("<strong>", Tree, "</strong>",
                                       "<br>Sampled_by:", Sampled_by,
-                                      "<br>Sampling_date:", Sampling_date),
+                                      "<br>Sampling_date:", Sampling_date,
+                                      "<br>Shipping_date:", Shipping_date),
                        color = ~pal()(data_filter()$Section),
                        fillOpacity = 0.8)
 
